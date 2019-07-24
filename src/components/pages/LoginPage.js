4980685c0345f4ac06dec2f0d6e5ebe8b5c9073b
DanielAguerrebere <daniel_aguerrebere@hotmail.com>
2019-07-24 12:49:14 -0500

    redux setup

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import LoginForm from "../forms/LoginForm";
+import {PropTypes} from "prop-types";
+import {connect} from "react-redux";
+import {login} from "../../actions/auth.js";
+
 
 class LoginPage extends React.Component {
 
     submit = (data) => {
-        console.log(data)
+        this.props.login(data).then(() => this.props.history.push("/"))
     };
 
     render() {
@@ -18,4 +22,11 @@ class LoginPage extends React.Component {
     }
 }
 
-export default LoginPage;
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+      push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(null, { login })(LoginPage);
